562506387ff6b7b45dccc49ed4cbcf87c2837f57
AMAN25800 <166643927+AMAN25800@users.noreply.github.com>
2024-11-16 22:27:44 +0530

    ItemRoute.js

--- route/ItemRoute.js
+++ route/ItemRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { addOrUpdateItems,listItems } from '../Controller/ItemsController.js'
 const itemRoute=express.Router();
-itemRoute.post('/additem',addItems);
+itemRoute.post('/additem',addOrUpdateItems);
 itemRoute.get('/listitems',listItems);
 export default itemRoute;

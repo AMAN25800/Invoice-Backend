43d66ba86dc8b8179e5a5a3f54e1706a0adcc67a
AMAN25800 <166643927+AMAN25800@users.noreply.github.com>
2024-11-18 18:32:43 +0530

    db.js

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 export const Connectdb=async()=>{
     const url=process.env.MONGODB_URL;
     await mongoose.connect(url,{
-        useNewUrlParser:true,useUnifiedTopology:true
+      
 
     })
     console.log("db connected ");
